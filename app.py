6433e1f79a9a1a152321b6d7b263f172b31bbc16
Nezuko <97461837+Riteeeeee@users.noreply.github.com>
2024-11-10 12:59:55 +0530

    added 'main' func call

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ def index():
 @socketio.on('message')
 def handle_message(msg):
     send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
