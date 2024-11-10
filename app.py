643f240d76ea6f1025b6c32da674b4c2085ba4f4
Nezuko <97461837+Riteeeeee@users.noreply.github.com>
2024-11-10 12:27:08 +0530

    test
    test commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(msg):
+    send(msg, broadcast=True)
