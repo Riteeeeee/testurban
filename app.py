59584391ffbe6e06eb0e8009109770e4ae3f0a07
Nezuko <97461837+Riteeeeee@users.noreply.github.com>
2024-11-10 13:12:49 +0530

    added port no.

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ def index():
 @socketio.on('message')
 def handle_message(msg):
     send(msg, broadcast=True)
-
+    
 if __name__ == '__main__':
-    socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)
